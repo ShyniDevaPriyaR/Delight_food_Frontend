f4b6a4be437c9f116d974482ce51b699f2582032
ShyniDevaPriyaR <shynidevapriya@gmail.com>
2025-12-24 13:46:44 +0530

    added styles for register

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -34,13 +34,13 @@ const Register = () => {
 
 
       <div className="register-container">
-        <div className="register-card">
+        <div className="register-card text-center">
           <h2 className='regtitle'>Create Account 📝</h2>
           <p className="regsubtitle">Register a new account</p>
 
-
+          <div className='d-flex justify-content-center'>
           <form onSubmit={handleSubmit}>
-            <label className='regsubtitle'>Username</label>
+            <label className='regsubtitle '>Username</label>
             <input
               type="text"
               className='form-control'
@@ -51,7 +51,7 @@ const Register = () => {
             />
 
 
-            <label className='regsubtitle'>Password</label>
+            <label className='regsubtitle mt-3'>Password</label>
             <input
               type="password"
               className='form-control'
@@ -62,15 +62,15 @@ const Register = () => {
             />
 
             
-            <button type="submit" className='regbtn'>Register</button>
+            <button type="submit" className='regbtn mt-3'>Register</button>
 
 
             {success && <p className="success">{success}</p>}
             {error && <p className="error">{error}</p>}
           </form>
+          </div>
 
-
-          <p className="login-text">
+          <p className="login-text mt-3">
             Already have an account? <Link to="/login">Login</Link>
           </p>
 
